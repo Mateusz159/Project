e494cf54bbdc0a38189c4f94d17c8e28486c8b13
8500129u@elektryk.edu.pl <8500129u@elektryk.edu.pl>
2024-09-17 11:22:43 +0200

    pierwszezmiany

---
+++ src/components/List/List.jsx
@@ -0,0 +1,18 @@
+import React  from "react"
+const list = () =>{
+    const list = [1,2,3,4,5,6]
+    const changelist = (list) => {
+        const tmp = list.map((x)=> x + 2)
+        return tmp = list.map((x)=> x+2)
+        return tmp
+    }
+    return<div>{list.map((x)=>{return x + ","})}<br/>{changelist(list).mapx + "}</div>
+}
+
+
+
+
+
+
+
+export default List 
